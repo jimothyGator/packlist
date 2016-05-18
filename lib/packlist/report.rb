05b3bac70c656ee28331a29e74980443e1bf7895
Jim Cushing <jimothy@mac.com>
2016-05-18 15:28:52 -0400

    render template using Padrino
    allows using Padrino helpers, such as partial.

--- lib/packlist/report.rb
+++ lib/packlist/report.rb
@@ -1,74 +1,18 @@
 require 'packlist/model'
 require 'packlist/utils'
-require 'erubis'
-require 'sass'
-require 'tilt'
-require 'erb'
+require 'packlist/template'
 
 module PackList
 	class Report
-		include ::PackList::GemUtils
+		include GemUtils
 
 		def initialize(packlist)
 			@packlist = packlist
-
-
-			template_path = File.join(templates_path('packlist'), 'report.html.erb')
-			# template_path = "report.html.erb"
-			@template = Tilt.new(template_path)
-			# input = File.read(template_path)
-			# @template = Erubis::Eruby.new(input)    # create Eruby object
+	        @template = Template.new templates_path('packlist'), 'report'
 		end
 
 		def save_to(filename)
-			css = read_stylesheet
-
-			context = {packlist: @packlist, styles: css, category_lists: [@packlist.categories]}
-
-			File.open(filename, 'w') do |file|
-				file.write(@template.render(nil, context))
-			end
-		end
-
-		private
-
-		def split_categories
-			categories = @packlist.categories.sort_by { |x| 0 - x.items.length }
-			cats1 = []
-			cats2 = []
-			count1 = 0
-			count2 = 0
-			categories.each do |cat|
-				if count2 < count1
-					cats2 << cat
-					count2 += cat.items.length + 2
-				else
-					cats1 << cat
-					count1 += cat.items.length + 2
-				end
-			end
-
-			category_lists = [cats1, cats2]
-			category_lists.each do |categories|
-				categories.sort_by! { |x| @packlist.categories.index(x) }
-			end
-
-			category_lists
-		end
-
-		def read_stylesheet
-			sass_options = {
-				syntax: :scss,
-				load_paths: [
-					stylesheets_path("packlist"),
-				]
-			}
-			template_path = File.join(stylesheets_path('packlist'), 'styles.scss')
-			template = File.read(template_path)
-			sass_engine = Sass::Engine.new(template, sass_options)
-			sass_engine.render
+			@template.render_to filename, {packlist: @packlist}
 		end
 	end
-
-
 end
